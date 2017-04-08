ae02da9dac1b9f262692fedce6b02cb58b1b634a
williamWang <251887063@qq.com>
2017-04-08 15:14:25 +0800

    movie项目简略框架
    vue 2.0初始化的项目，加了mint-ui ui框架，具体的有想用的自己添加依赖

---
+++ tmovie/src/components/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="index">
+    电影首页
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+      };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  };
+</script>
+<style scoped>
+  .index{
+    background: #F4F4F4;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
